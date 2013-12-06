f4dcf8c25c3eaf1857cdd8f163b7ca01ae551d8e
Benjamin Pott <benjamin.pott@advertstream.com>
2013-12-06 10:51:59 +0100

    ajout d'une demo jsfiddler

--- example/demo.js
+++ example/demo.js
@@ -1,6 +1,5 @@
 window.addEvent('domready', function() {
 	new Request.HTML({
-		//url: 'http://jsfiddle.net/gh/get/benjipott/video.js-dailymotion/tree/master/example/',
 		url: 'http://jsfiddle.net/gh/get/benjipott/video.js-dailymotion/tree/master/lib/video.js',
 		data: {'delay': 1},
 		method: 'post',
